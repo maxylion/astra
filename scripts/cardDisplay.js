b5a2ce70d35ca7fe38ebb5b4a82e84c4328637b3
maxylion <theottermax@gmail.com>
2023-02-26 16:31:45 -0300

    somewhat implement side changing

--- scripts/cardDisplay.js
+++ scripts/cardDisplay.js
@@ -1,4 +1,8 @@
-
+const sides = {
+	mukaki:"devil",
+	makari:"god"
+}
+let side = sides.makari;
 function load(){
 	document.getElementById("display").style.display = "none";
 }
@@ -7,8 +11,9 @@ function changeCard(cardName) {
 	fetch("./scripts/resources/cards.json")
 	.then((res) => res.json())
 	.then((data) => {
-		var img = new Image();
-		img.src = data[cardName]["src"];
+		console.log(`src_${side}`);
+		let img = new Image();
+		img.src = data[cardName][`src_${side}`];
 		document.getElementById("cardimgfull").src = img.src;
 		document.getElementById("abil").textContent = `Active Ability: ${data[cardName]["info"]["ability"]}`
 		document.getElementById("abil2").textContent = `Passive Ability: ${data[cardName]["info"]["passive"]}`
@@ -16,4 +21,22 @@ function changeCard(cardName) {
 		document.getElementById("title").textContent = data[cardName]["name"];
 		document.getElementById("title2").textContent = data[cardName]["title"]
 	});
+}
+function changeSide(){
+	switch(side){
+		case sides.makari:
+			side = sides.mukaki;
+			break;
+		case sides.mukaki:
+			side = sides.makari;
+			break;
+	};
+	fetch('./scripts/resources/cards.json')
+	.then((res) => res.json())
+	.then((data => {
+		let keys = Object.keys(data);
+		keys.forEach(name => {
+			changeCard(name);
+		});
+	}))
 }
