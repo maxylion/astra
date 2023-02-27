7d446e29f879c8d6c0aebecde5f6ad6f71f5e01e
maxylion <theottermax@gmail.com>
2023-02-27 17:59:26 -0300

    prepare for spell card reference

--- scripts/cardDisplay.js
+++ scripts/cardDisplay.js
@@ -1,8 +1,9 @@
 const sides = {
 	mukaki:"devil",
 	makari:"god"
-}
+};
 let currentCard = "";
+let color = "";
 let side = sides.makari;
 function load(){
 	document.getElementById("cardimgfull").style.visibility = "hidden";
@@ -10,6 +11,7 @@ function load(){
 }
 function changeCard(cardName) {
 	currentCard = cardName;
+	document.getElementById("colorselect").style.display = "none";
 	document.getElementById("cardimgfull").style.visibility = "visible";
 	fetch("./scripts/resources/cards.json")
 	.then((res) => res.json())
@@ -22,8 +24,29 @@ function changeCard(cardName) {
 		document.getElementById("abil2").textContent = `Passive Ability: ${info["passive"]}`
 		document.getElementById("cost").textContent = `Summon cost: ${data[cardName]["cost"]}`
 		document.getElementById("title").textContent = data[cardName]["name"];
-		document.getElementById("title2").textContent = data[cardName]["title"]
+		document.getElementById("title2").textContent = data[cardName]["title"];
 	});
+}
+function changeColor(newColor){
+	color = newColor;
+}
+function changeSpell(spellName){
+	currentCard = spellName;
+	document.getElementById("colorselect").style.display = "";
+	document.getElementById("cardimgfull").style.visibility = "visible";
+	fetch("./scripts/resources/spells.json")
+	.then((res) => res.json())
+	.then((data) => {
+		let img = new Image();
+		let name = data[cardName]["name"];
+		let desc = data[cardName]["desc"];
+		img.src = data[cardName]["srcs"][color];
+		document.getElementById("cardimgfull").src = img.src;
+		document.getElementById("abil").textContent = `Ability: ${desc}`;
+		document.getElementById("abil2").textContent = "";
+		document.getElementById("title").textContent = name;
+		document.getElementById("title2").textContent = "Spell Card";
+	})
 }
 function changeSide(){
 	let button = document.getElementById("side");
